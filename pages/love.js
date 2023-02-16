0e0a5e8e2d537531948c29a55885dcab3d8096b6
Arushigandhi <arushi.gandhi@gmail.com>
2023-02-17 01:02:25 +0530

    love text

--- pages/love.js
+++ pages/love.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import styles from "../styles/Home.module.scss";
@@ -6,21 +5,80 @@ import one from "./assets/goa.jpeg";
 import two from "./assets/beach.jpeg";
 import three from "./assets/nindhi.jpeg";
 import { Row } from "antd";
+import Image from "next/image";
 
 export default function love() {
   return (
     <>
       <div className={styles.aboutContainer}>
-        <div className={styles.aboutImage}>
-          <Image src={one} alt="arushi" width={700} height={550} />
-        </div>
-        <div className={styles.aboutDescription}>
-          <h1>Dear Mr Aryamann Ningombam,</h1>
+        <div className={styles.aboutDescription} style={{ margin: "2rem 0" }}>
+          <div className={styles.aboutImage}>
+            <Image src={one} width={550} height={900} alt="nindhi" />
+          </div>
+          <h1>Hello meri jaan,</h1>
           <h2>
-            I love you. And I really miss you stupid bitch. You are my favourite
-            person on earth and I could talk to you for 7 janam , round figure,
-            still won`t be enough. Meet me asap. (I shall update this easter egg
-            section soon.)
+            There's not much that I haven't told you about. When it comes to
+            expressing all my love to Aryamann Ningombam, I'd like to believe I
+            have a PhD. in that. I love you. Sooo much. And, I miss you even
+            more. I miss our days together, horsing around campus, sitting in
+            secluded spots and chatting. Me coming to you every hour in IC just
+            to see you, give you a hug, give you a kiss, make obscene jokes,
+            annoy you, flirt with you, sleep on your godi (the fact that i
+            literally slept in your lap while you coded is sooo funny now that I
+            think about it. Peak moments man I miss that) How is it that I could
+            spend the entire day with you and still have gazillion things to say
+            and never get enough of you. I miss us eating food together and
+            planning dates and booking hotels. I sooo miss holding your arm
+            while we just walked to class or some shit, or how I waited for
+            breaks between classes to see you for 15 mins. I was thinking about
+            how you used to ekdam se kiss me outside coworking door in the dark
+            and pick me up and shit. So hot. Goals bro we are. I miss you
+            scolding me for not fixing Spotify and making me fix it in the
+            middle of Apoorva. You probably don't even know why I was so mad at
+            you for doing that. It's because I wanted to sit with you in some
+            shed and make out and cuddle but noooo you want to fix my Spotify
+            instead of fix each other's orgasms. I miss me bringing my entire
+            laptop and mouse and shit to you everytime there was a tech error
+            outside my brain's bandwidth and I love how patiently you helped me.
+            <br /> <br />I think the thing I probably find the funniest is that
+            I ever though we'd get bored of each other. I can't get enough of
+            spending time with you and discusing and planning our future
+            together. The fact that I have to wait sooo long to finally see you
+            is tooooooo fucking annoying to deal with. Honestly dekha jaye toh
+            teri hi galti hai ki bola expectations mat rakh, bitch don't you
+            know meee cause no matter how much I try, I'll always want to see
+            you and see your dick, more importantly. Honestly, you know what you
+            need to stop going to the gym and getting so hot. My answer is two
+            fold: 1) Other bitches get to see you and I don't and that is a
+            crime. 2) It makes me horny, esp in the gym thinking about gymming
+            with you in that white tank top and grey shorts and damn.... why
+            can't you make that happen huh itna bolta hai im aryamann im
+            aryamann, yeh karke dikha fir karna
+            <br /> <br />
+            You're my greatest inspiration Aryamann. Everything you do, say,
+            feel, buy, sell, have an opinion on, matters to me. It matters too
+            much. And I take it personally bro ahahahah. Today, you were like
+            I'm going to buy a watch and I took that personally lmao. I was like
+            kyun yeh humara paise kharch raha hai bc. Everything you do matters
+            to me my love. I love how we are right now and I can't wait for our
+            life together to get started someday. If you think about it, it's
+            good only we have to do LDR, karma will make it such that because we
+            had to spend time apart rn so we'll be together for the rest of our
+            7 janam hihihahah. I'm going to 10-12 hours in a day all up in your
+            face in one tiny 1-bhk for several years, I'm going to be there when
+            you poop, when you brush, when you make eggs, when you pack your
+            bag, when we have sex, when we cuddle, when we watch a movie in bed,
+            when we order takeout, when we plan our Maldives trip, when we plan
+            our financials, when we get hottest entrepreneur jodi of the year
+            award, everything basically whore. You should be very scared, I'm
+            not going to give you any personal space, lauda mera "me time", only
+            arushi time, I'll buy a 2xl tshirt and sleep in it with you bro
+            watch, full invasion of you and your dick.
+            <br /> <br />I think that is enough love for now. Otherwise, I'll
+            start crying again. Just know my love for you is more than
+            Kilimanjaro.
+            <br /> <br />
+            Missing you always, The hottest bitch you'll ever fuck 😘
           </h2>
         </div>
       </div>
